6c988256a473525f25b4a1098536ea4133c08323
emilkanerva <100346020+emilkanerva@users.noreply.github.com>
2025-09-30 13:34:01 +0200

    test setup

--- tsunami/intro/background.sh
+++ tsunami/intro/background.sh
@@ -7,3 +7,9 @@ if ! systemctl is-active --quiet docker; then
     systemctl start docker
 
 
+# Setup the vulnerable web server (automatically)
+
+echo "FROM httpd:2.4.49
+COPY ./public-html/ /usr/local/apache2/htdocs/
+COPY ./my-httpd.conf /usr/local/apache2/conf/httpd.conf" > webserver.Dockerfile
+
