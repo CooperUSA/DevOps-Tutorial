1f0f9a0aff640057a75ce861544f87638b81721e
emilkanerva <100346020+emilkanerva@users.noreply.github.com>
2025-09-30 13:50:51 +0200

    it worky but change name questionmark?

--- tsunami/intro/background.sh
+++ tsunami/intro/background.sh
@@ -10,7 +10,7 @@ echo "[*] Starting background setup..."
 # Setup the vulnerable web server (automatically)
 echo "[*] setting up web server"
 
-cat > Dockerfile << EOF
+cat > webserver.Dockerfile << EOF
 FROM httpd:2.4.49
 COPY ./public-html/ /usr/local/apache2/htdocs/
 COPY ./my-httpd.conf /usr/local/apache2/conf/httpd.conf
