f83f49a3a41491f92328f6ea585eebfcc5e50371
emilkanerva <100346020+emilkanerva@users.noreply.github.com>
2025-10-02 13:14:01 +0200

    add commands to step 6

--- tsunami/step6/background.sh
+++ tsunami/step6/background.sh
@@ -3,7 +3,24 @@ cp -r public-html repo/public-html
 cp httpd.conf repo
 cp webserver.Dockerfile repo
 mkdir repo/.github
-mkdir repo/.github/workflow
+mkdir repo/.github/workflows
 cd repo
 git init
 git branch -m main
+
+
+cat > ./.github/workflows/tsunami.yml << EOF
+name: Check web server for vulnerability using tsunami
+
+on:
+  push:
+    branches:
+      - main
+
+jobs: 
+  everything: 
+    runs-on: ubuntu-latest
+    
+EOF
+
+
