2eb6ffd9f69e76aaf356045c23fc1f6800eb81a5
emilkanerva <100346020+emilkanerva@users.noreply.github.com>
2025-10-03 13:44:11 +0200

    different step 4 verify

--- tsunami/step4/verify.sh
+++ tsunami/step4/verify.sh
@@ -2,4 +2,8 @@ if [ -f "/tmp/skip" ]; then
   exit 0
 fi
 
-head -n 1 webserver.Dockerfile | grep -v "2.4.49\|2.4.50"
+if [ "$(curl http://localhost:8080/cgi-bin/.%2e/.%2e/.%2e/.%2e/.%2e/etc/passwd | grep 'Gnats Bug-Reporting System')" ]; then
+  exit 1
+else
+  exit 0
+fi
